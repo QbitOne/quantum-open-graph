1bfd1f534c7edd98dcfeb76fb95fc40f6bd75103
Andreas Geyer <andreas@qbitone.de>
2021-05-21 18:13:27 +0200

    set version to 0.1.1

--- quantum-open-graph.php
+++ quantum-open-graph.php
@@ -8,7 +8,7 @@
  * Author URI:      https://qbitone.de
  * Text Domain:     quantum-open-graph
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         0.1.1
  *
  * @package         Quantum_Open_Graph
  */
